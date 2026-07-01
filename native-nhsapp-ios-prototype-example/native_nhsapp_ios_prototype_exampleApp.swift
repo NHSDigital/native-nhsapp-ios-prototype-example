7bd59d31a8b10bcf1eb0fe39c85fb0bf413a35e0
Frankie Roberto <frankie.roberto1@nhs.net>
2026-07-01 09:40:45 +0100

    Initial Commit

---
+++ native-nhsapp-ios-prototype-example/native_nhsapp_ios_prototype_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_nhsapp_ios_prototype_exampleApp.swift
+//  native-nhsapp-ios-prototype-example
+//
+//  Created by Frankie Roberto on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct native_nhsapp_ios_prototype_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
